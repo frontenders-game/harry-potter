d13b0acb0adfb7ec8d5605f5660291a5281cace6
frontenders_game <sounnecessary83@gmail.com>
2023-02-02 21:12:17 +0300

    add modal card

--- js/main.js
+++ js/main.js
@@ -1,127 +1,115 @@
 import data from "./potterdb/data.js";
+import {modalCardHTML, siteCardHTML} from "./process_cards.js";
 
 
-const main = () => {
-    // load json with character cards
-    const charactersData = JSON.parse(JSON.stringify(data))
+// load json with character cards
+const charactersData = JSON.parse(JSON.stringify(data))
 
-    // select container for character cards
-    const cardContainer = document.querySelector('div.main-wrapper')
+// query Selectors
+// main div with characters
+const cardContainer = document.querySelector('div.main-wrapper')
+// div with search input and selector
+const searchForm = document.querySelector('.search-wrapper')
+// name search
+const nameForm = document.querySelector('.search__name-input')
+// house selector
+const houseSelector = document.querySelector('.search__house-selector')
+// modal
+const modalOverlay = document.querySelector(".modal-overlay")
+const modalContainer = document.querySelector(".modal-character-wrapper")
 
-    // div with characters
-    const clearCharacters = () => cardContainer.innerHTML = ''
+const clearCharacters = () => cardContainer.innerHTML = ''
 
-    // div with search input and selector
-    const searchForm = document.querySelector('.search-wrapper')
-
-    // name search
-    const nameForm = document.querySelector('.search__name-input')
-
-    // house selector
-    const houseSelector = document.querySelector('.search__house-selector')
-
-    const houseList = () => {
-        const houses = data.reduce((acc, current) => acc.add(current.attributes.house), new Set())
-        return [...houses].filter(house => !!house).sort()
-    }
-
-    // iterate through data and fill house array
-    const fillHouses = () => {
-        const createOption = (optionText, optionValue) => {
-            const option = document.createElement("option");
-            option.setAttribute("value", optionValue);
-            option.textContent = optionText
-            houseSelector.appendChild(option)
-        }
-        createOption('All schools', '')
-        for (let house of houseList()) {
-            createOption(house, house)
+const getCharacterById = id => {
+    for (let character of charactersData) {
+        if (character.id === id) {
+            return character
         }
     }
+}
 
-    // add houses to selector
-    fillHouses()
+const houseList = () => {
+    const houses = data.reduce((acc, current) => acc.add(current.attributes.house), new Set())
+    return [...houses].filter(house => !!house).sort()
+}
 
-    // function to render each card
-    const createCharacterCard = characterObj => {
-        // {
-        //   "id": "17a9946f-edf3-4731-97c1-e4cc7a664db5",
-        //     "type": "character",
-        //     "attributes": {
-        //       "slug": "witch-and-wizard-couple",
-        //       "name": "Witch and Wizard Couple",
-        //       "born": null,
-        //       "died": "Between 1485 and 1603",
-        //       "gender": "Male, Female",
-        //       "species": "Humans",
-        //       "height": null,
-        //       "weight": null,
-        //       "hair_color": "Brown",
-        //       "eye_color": null,
-        //       "skin_color": "Light",
-        //       "blood_status": null,
-        //       "marital_status": "Married (possibly)",
-        //       "nationality": null,
-        //       "animagus": null,
-        //       "boggart": null,
-        //       "house": null,
-        //       "patronus": null,
-        //       "alias_names": null,
-        //       "family_members": null,
-        //       "jobs": null,
-        //       "romances": null,
-        //       "titles": null,
-        //       "wands": null,
-        //       "image": "https://static.wikia.nocookie.net/harrypotter/images/a/ab/Portrait_of_a_medieval_couple.jpg",
-        //       "wiki": "https://harrypotter.fandom.com/wiki/Witch_and_Wizard_Couple"
-        //    }
-        // }
-        const {image, name, species, gender, house, wands, died, born} = characterObj.attributes
-
-        const fmtWands = wands.join(', ')
-        const card = document.createElement('div')
-        card.className = 'character-card'
-        card.innerHTML = `
-                      <div class="character-wrapper">
-                      <div class="character-image-wrapper">
-                        <img class="character__image" src="${image}" alt="image of ${name}">
-                      </div>  
-                        <div class="character-text-wrapper">
-                          <h3 class='character__title'>${name}</h3>
-                          <p><b>Species</b>: ${species}</p>
-                          <p><b>Gender</b>: ${gender}</p>
-                          <p><b>House</b>: ${house || 'Unknown'}</p>
-                          <p><b>Wands</b>: ${fmtWands}</p>
-                          <p><b>Born</b>: ${born || 'Date unknown'}</p>
-                          <p><b>Died</b>: ${died || 'Alive'}</p>
-                        </div>
-                     </div>`
-        return card
-
+// iterate through data and fill house array
+const fillHouses = () => {
+    const createOption = (optionText, optionValue) => {
+        const option = document.createElement("option");
+        option.setAttribute("value", optionValue);
+        option.textContent = optionText
+        houseSelector.appendChild(option)
     }
+    createOption('All houses', '')
+    for (let house of houseList()) {
+        createOption(house, house)
+    }
+}
 
-    // function to generate html from array with json
-    const renderFromData = dataArr => dataArr.forEach(character => cardContainer.append(createCharacterCard(character)))
+// create modal html
 
-    // Initial generation of all character cards
-    renderFromData(charactersData)
+const modalOpen = (evt) => {
+    document.body.style.overflow = 'hidden'
+    modalOverlay.classList.add('visible')
+    modalContainer.classList.add('visible')
+    const character = getCharacterById(evt.target.id)
+    modalContainer.innerHTML = modalCardHTML(character)
+    const closeModalBtn = document.querySelector(".modal__close-btn")
+    closeModalBtn.addEventListener('click', () => modalClose())
+    modalOverlay.addEventListener('click', () => modalClose())
+    document.addEventListener('keydown', (evt) => {
+        if (evt.code === 'Escape' || evt.code === 'Enter') modalClose()
+    })
+}
+
+const modalClose = () => {
+    document.body.style.overflow = 'visible'
+    modalOverlay.classList.remove('visible')
+    modalContainer.classList.remove('visible')
+    modalContainer.innerHTML = ''
+}
+
+// function to render each card
+const createCharacterCard = characterObj => {
+    const card = document.createElement('div')
+    card.className = 'character-wrapper'
+    card.innerHTML = siteCardHTML(characterObj)
+    const titleURL = card.querySelector('.character__url')
+    titleURL.addEventListener('click', evt => {
+        modalOpen(evt)
+    });
+    return card
+}
 
 
+// function to generate html from array with json
+const renderFromData = dataArr => dataArr.forEach(
+    characterObj => cardContainer.append(createCharacterCard(characterObj)))
+
+const addSearchListener = () => {
     searchForm.addEventListener('input', () => {
         const fmtName = nameForm.value.toLowerCase().trim()
-
         const house = houseSelector.value
-
         const filteredCharacters = charactersData
-            .filter(character => character.attributes.name.toLowerCase().includes(fmtName))
-            .filter(character => {
-                const characterHouse = character.attributes.house || 'Unknown'  // "house": null =>  "house": "Unknown"
+            .filter(characterObj => characterObj.attributes.name.toLowerCase().includes(fmtName))
+            .filter(characterObj => {
+                const characterHouse = characterObj.attributes.house || 'Unknown'
                 return characterHouse === house || !house
             })
-
         clearCharacters()
         renderFromData(filteredCharacters)
     })
+}
+
+const main = () => {
+    // add houses to selector
+    fillHouses()
+    // Initial generation of all character cards
+    renderFromData(charactersData)
+    // activate search form listener
+    addSearchListener()
+
 }
 
 main()
---
+++ js/process_cards.js
@@ -0,0 +1,105 @@
+const KEYS = ['born', 'died', 'gender', 'species', 'house', 'height', 'weight', 'hair_color', 'eye_color', 'skin_color', 'blood_status', 'marital_status', 'nationality', 'animagus', 'boggart', 'patronus', 'alias_names', 'family_members', 'jobs', 'romances', 'titles', 'wands', 'wiki']
+
+const SHORT_KEYS = ['gender', 'species', 'house', 'wands', 'born', 'died']
+
+const processKey = key => key[0].toUpperCase() + key.slice(1).replace('_', ' ')
+
+const processAttributes = (key, data) => {
+    if (Array.isArray(data)) {
+        return data.join(', ')
+    } else {
+        if (key === 'house') return data || 'Unknown'
+        else if (key === 'born') return data || 'Date unknown'
+        else if (key === 'died') return data || 'Alive'
+        else if (key === 'wiki') {
+            return `<a class="character__url wand-cursor" href="${data}" id="" target="_blank">url</a>`
+        }
+
+    }
+    return data
+}
+
+const generateCardAttributes = (characterObj, keys) => {
+    // {
+    //     "id": "8c43c796-2e42-4a81-8d37-447a02e4235a",
+    //     "type": "character",
+    //     "attributes": {
+    //         "slug": "harry-potter",
+    //         "name": "Harry James Potter",
+    //         "born": "31 July 1980, Godric's Hollow, West Country, England, Great Britain",
+    //         "died": null,
+    //         "gender": "Male",
+    //         "species": "Human",
+    //         "height": null,
+    //         "weight": null,
+    //         "hair_color": "Jet-black",
+    //         "eye_color": "Bright green",
+    //         "skin_color": "Light",
+    //         "blood_status": "Half-blood",
+    //         "marital_status": "Married",
+    //         "nationality": "English",
+    //         "animagus": null,
+    //         "boggart": "Dementor",
+    //         "house": "Gryffindor",
+    //         "patronus": "Stag",
+    //         "alias_names": ["The Boy Who Lived", "The Chosen One", "Undesirable No. 1", "Lightning
+    //                          (by Potterwatch)", "The Boy Who Lied (by the Daily Prophet)", "Gregory Goyle
+    //                          (under disguise of Polyjuice Potion)"],
+    //         "family_members": ["James Potter I (father) †", "Lily J. Potter (née Evans) (mother) †",
+    //                            "Ginevra Potter (née Weasley) (wife)", "James Potter II (son)"]
+    //         "jobs": ["Head of British Auror Office (formerly)",
+    //                  "Guest lecturer on Defence Against the Dark Arts"],
+    //         "romances": ["Cho Chang (ex-girlfriend)", "Ginevra Potter (wife)"],
+    //         "titles": ["Triwizard Champion", "Seeker", "Quidditch Captain", "Master of Death"],
+    //         "wands": ["11', Holly, phoenix feather", "10¾', Vine, dragon heartstring (temporarily)",
+    //                   "10', Blackthorn, unknown core (temporarily)"],
+    //         "image": "https://static.wikia.nocookie.net/harrypotter/images/9/97/Harry_Potter.jpg",
+    //         "wiki": "https://harrypotter.fandom.com/wiki/Harry_Potter"
+    //     }
+    // }
+
+    const attrs = characterObj.attributes
+
+    const attrsResult = []
+    for (let key of keys) {
+        const value = attrs[key]
+        if (value || key === 'house') {
+            attrsResult.push(`<p><b>${processKey(key)}</b>: ${processAttributes(key, value)}</p>`)
+        }
+    }
+    return attrsResult.join('\n')
+
+}
+
+
+export const siteCardHTML = characterObj => {
+    const attrs = characterObj.attributes
+    const attributes = generateCardAttributes(characterObj, SHORT_KEYS)
+    const title = `<a href="#" id="${characterObj.id}" class="character__url wand-cursor">${attrs.name}</a>`
+
+    return `<div class="character-image-wrapper">
+                <img class="character__image" src="${attrs.image}" alt="image of ${attrs.name}">
+            </div>
+            <div class="character-text-wrapper">
+                <h3 class='character__title'>${title}</h3>
+                ${attributes}
+            </div>`
+}
+
+export const modalCardHTML = characterObj => {
+    const attrs = characterObj.attributes
+    const attributes = generateCardAttributes(characterObj, KEYS)
+
+    return `<div class="modal__content-no-scroll">
+                    <div class="modal__image-wrapper">
+                        <img class="character__image" src="${attrs.image}" alt="image of ${attrs.name}">
+                    </div>  
+                    <h3 class='modal-character__title'>${attrs.name}</h3>
+                </div>
+                <div class="modal__-text-wrapper">
+                    ${attributes}
+                </div>  
+                <button class="modal__close-btn wand-cursor">[ X ]</button>
+            </div>`
+}
+
